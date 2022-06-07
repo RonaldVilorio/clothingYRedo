4a78b89e7ed8dc7050771b3f7c446fb58ae4c67f
RonaldVilorio <ronvilorio@gmail.com>
2022-06-07 10:16:43 -0400

    added home/shop routes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,18 @@
-import CardList from "./components/cardlist/cardlist.component"
-
-
-const cardlist =[
-  {
-    "id": 1,
-    "title": "hats",
-    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-  },
-  {
-    "id": 2,
-    "title": "jackets",
-    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-  },
-  {
-    "id": 3,
-    "title": "sneakers",
-    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-  },
-  {
-    "id": 4,
-    "title": "womens",
-    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-  },
-  {
-    "id": 5,
-    "title": "mens",
-    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-  }
-]
-
+import Home from "./routes/home/home.component";
+import Shop from "./routes/shop/shop.component";
+import { Routes,Route } from "react-router-dom";
+import { Fragment } from "react";
 const App =()=> {
   return (
-    <div>
-      <CardList cardlist={cardlist}/>
-    </div>
+    <Fragment>
+      <Routes>
+        <Route path="/" element={<Home/>}>
+          <Route path="/shop" element={<Shop/>}/>
+
+        </Route>
+      </Routes>
+    </Fragment>
+    
   );
 }
 
---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,8 @@
+
+const Shop = () => {
+    return ( 
+        <div>Shop Now</div>
+     );
+}
+ 
+export default Shop;
