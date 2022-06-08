c88ea43ff11bdd0e73a08978f6154314e49bb3b3
RonaldVilorio <ronvilorio@gmail.com>
2022-06-08 13:21:34 -0400

    sign up form value code done

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,18 +1,34 @@
+import { useState } from "react";
+
+const defaultFormFields = {
+    displayName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+}
 
 const SignUpForm = () => {
+    const [formFields,setFormFields] = useState(defaultFormFields)
+    const {displayName,email,password,confirmPassword} = formFields
+    
+    const handleChange = (e) => {
+        const {name,value} = e.target
+        setFormFields({...formFields,[name]:value})
+    }
     return ( 
         <form onSubmit={()=>{}}>
             <label>Display Name</label>
-            <input type="text" required />
+            <input type="text" onChange={handleChange} name='displayName' value={displayName} required />
 
             <label>Email</label>
-            <input type="email" minLength={6} required />
+            <input type="email" onChange={handleChange} name='email' value={email} minLength={6} required />
 
             <label>Password</label>
-            <input type="password" required />
+            <input type="password" onChange={handleChange} name='password' value={password} required />
 
             <label>Confirm Password</label>
-            <input type="password" required />
+            <input type="password" onChange={handleChange} name='confirmPassword' value={confirmPassword} required />
+
             <button type="submit">Sign Up</button>
         </form>
      );
