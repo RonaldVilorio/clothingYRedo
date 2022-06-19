1bbca9b4ea94cb98839ca6460033ccaec890b816
RonaldVilorio <ronvilorio@gmail.com>
2022-06-19 15:37:10 -0400

    added sign-up styles file

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { createAuthUserWithEmailAndPassword,createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
 import FormInput from "../form-input/form-input.component";
+import './sign-up.component.styles.scss'
+
 const defaultFormFields = {
     displayName: '',
     email: '',
@@ -39,18 +41,24 @@ const SignUpForm = () => {
         }
         resetFormFields()
     }
-    return ( 
-        <form onSubmit={handleSubmit}>
-            <FormInput label='Display Name' type="text" onChange={handleChange} name='displayName' value={displayName} required />
+    return (
+        <div className='sign-up-container'>
+            <h2>Don't have an account?</h2>
+            <span>Sign up with your email and password</span>
 
-            <FormInput label='Email' type="email" onChange={handleChange} name='email' value={email} minLength={6} required />
+            <form onSubmit={handleSubmit}>
+                <FormInput label='Display Name' type="text" onChange={handleChange} name='displayName' value={displayName} required />
 
-            <FormInput label='Password'type="password" onChange={handleChange} name='password' value={password} required />
+                <FormInput label='Email' type="email" onChange={handleChange} name='email' value={email} minLength={6} required />
 
-            <FormInput label='Confirm Password'type="password" onChange={handleChange} name='confirmPassword' value={confirmPassword} required />
+                <FormInput label='Password'type="password" onChange={handleChange} name='password' value={password} required />
 
-            <button type="submit">Sign Up</button>
-        </form>
+                <FormInput label='Confirm Password'type="password" onChange={handleChange} name='confirmPassword' value={confirmPassword} required />
+
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
+        
      );
 }
  
